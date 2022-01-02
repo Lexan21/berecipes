9fe8b3a2fafb382ed2ea81a1ff6978c5584afff3
Alx <alexandra8taran52sm@gmail.com>
2022-01-02 13:25:00 +0200

    added @mui/material and @mui/icons-material libraries

---
+++ src/container/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import CssBaseline from '@material-ui/core/CssBaseline'
+import Header from './Header/Header'
+import Main from './Main/Main'
+import Footer from './Footer/Footer'
+import CssBaseline from '@mui/material/CssBaseline';
+// import Headerer from './Headerer/Headerer'
+
+
+
+const App = () => {
+    return (
+        <div className="body">
+            <CssBaseline />
+            <Header />
+            {/* <Headerer /> */}
+            <Main />
+            <Footer />
+
+        </div>
+    )
+}
+
+export default App
